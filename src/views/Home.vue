dd10d6c9add563624e53d6430629071a162c064b
Muhammad kautsar Panggawa <49815027+sarunk21@users.noreply.github.com>
2021-06-17 22:33:05 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,6 +27,6 @@ import Form from '../components/content/FormContact.vue'
 
 export default {
   name: "Home",
-  components: { Navbar, Hero, Map, Footer, Service, Footer, ContactUs,  Work, Form, Review },
+  components: { Navbar, Hero, Map, Footer, Service, Footer, ContactUs, Team, Work, Form, Review },
 };
 </script>
